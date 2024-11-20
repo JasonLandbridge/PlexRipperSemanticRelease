6f42ee9377cf19ce78d33306d0d4122e498dc78f
JasonLandbridge <jasonlandbridge@protonmail.com>
2024-11-20 20:10:28 +0100

    feat(Web-UI): The question if you want to start the setup process is now a dialog pop-up window

--- src/WebAPI/ClientApp/src/layouts/default.vue
+++ src/WebAPI/ClientApp/src/layouts/default.vue
@@ -23,6 +23,7 @@
 			:key="alertItem.id"
 			:alert="alertItem" />
 		<CheckServerConnectionsDialog />
+		<FirstTimeSetupDialog />
 		<!--	Background	-->
 		<Background :hide-background="isEmptyLayout" />
 	</q-layout>
@@ -33,12 +34,22 @@ import Log from 'consola';
 import { useSubscription } from '@vueuse/rxjs';
 import { get, set } from '@vueuse/core';
 import type { IAlert } from '@interfaces';
-import { useHelpStore, useAlertStore, useGlobalStore, useDialogStore, useRoute, nextTick } from '#imports';
+import { DialogType } from '@enums';
+import {
+	useHelpStore,
+	useAlertStore,
+	useGlobalStore,
+	useDialogStore,
+	useSettingsStore,
+	useRoute,
+	nextTick,
+} from '#imports';
 
 const route = useRoute();
 const helpStore = useHelpStore();
 const alertStore = useAlertStore();
 const dialogStore = useDialogStore();
+const settingsStore = useSettingsStore();
 
 const alerts = ref<IAlert[]>([]);
 const showNavigationDrawerState = ref(true);
@@ -57,6 +68,10 @@ function toggleNotificationsDrawer() {
 }
 
 onMounted(() => {
+	if (settingsStore.generalSettings.firstTimeSetup) {
+		dialogStore.openDialog(DialogType.FirstTimeSetupDialog);
+	}
+
 	useSubscription(
 		useGlobalStore().getPageSetupReady.subscribe({
 			next: () => {
--- src/WebAPI/ClientApp/src/pages/index.vue
+++ src/WebAPI/ClientApp/src/pages/index.vue
@@ -1,31 +1,5 @@
 <template>
 	<QPage>
-		<QRow v-if="settingsStore.generalSettings.firstTimeSetup">
-			<QCol cols="12">
-				<QText
-					align="center"
-					size="h5"
-					class="q-my-lg">
-					{{ $t('pages.home.setup-question') }}
-				</QText>
-				<QRow
-					justify="center"
-					gutter="md">
-					<QCol cols="3">
-						<NavigationSkipSetupButton
-							block
-							@click="skipSetup()" />
-					</QCol>
-					<QCol cols="3">
-						<GoToButton
-							:label="$t('general.commands.go-to-setup-page')"
-							block
-							to="/setup"
-							color="positive" />
-					</QCol>
-				</QRow>
-			</QCol>
-		</QRow>
 		<MediaOverview
 			:library-id="0"
 			:media-type="settingsStore.displaySettings.allOverviewViewMode" />
@@ -33,13 +7,7 @@
 </template>
 
 <script setup lang="ts">
-import Log from 'consola';
-import { useSettingsStore } from '@store';
+import { useSettingsStore } from '#imports';
 
 const settingsStore = useSettingsStore();
-
-const skipSetup = () => {
-	Log.info('Setup process skipped');
-	settingsStore.generalSettings.firstTimeSetup = false;
-};
 </script>
